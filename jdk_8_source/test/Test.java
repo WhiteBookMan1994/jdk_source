d927def6f2a38f0e2fb267e67e006279b259008d
dingchenchen <>
2020-04-26 14:28:11 +0800

    jdk8源码添加

---
+++ jdk_8_source/test/Test.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+/**
+ * @author dingchenchen
+ * @since 2020-04-26
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        String s = "hello world!";
+        HashMap map = new HashMap();
+        System.out.println(map.size());
+    }
+}
