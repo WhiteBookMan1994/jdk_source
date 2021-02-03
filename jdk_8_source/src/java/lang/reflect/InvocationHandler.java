29c42f73fe43b1acd3ead44f69434cbc72b3049c
dingchenchen <1936886538@qq.com>
2021-02-03 08:15:06 +0800

    动态代理相关Proxy类和InvocationHandler接口源码学习

--- jdk_8_source/src/java/lang/reflect/InvocationHandler.java
+++ jdk_8_source/src/java/lang/reflect/InvocationHandler.java
@@ -29,10 +29,16 @@ package java.lang.reflect;
  * {@code InvocationHandler} is the interface implemented by
  * the <i>invocation handler</i> of a proxy instance.
  *
+ * InvocationHandler 是代理实例的调用处理程序 实现的接口。
+ * （代理实例：是代理类的一个实例，Proxy的子类实例）
+ *
  * <p>Each proxy instance has an associated invocation handler.
  * When a method is invoked on a proxy instance, the method
  * invocation is encoded and dispatched to the {@code invoke}
  * method of its invocation handler.
+ *
+ * 每个代理实例都具有一个关联的调用处理程序。
+ * 对代理实例调用方法时，将对方法调用进行编码并将其指派到它的调用处理程序的 invoke 方法。
  *
  * @author      Peter Jones
  * @see         Proxy
@@ -46,13 +52,19 @@ public interface InvocationHandler {
      * when a method is invoked on a proxy instance that it is
      * associated with.
      *
+     * 在代理实例上处理方法调用并返回结果。
+     * 在与方法关联的代理实例上调用方法时，将在调用处理程序上调用此方法。
+     *
      * @param   proxy the proxy instance that the method was invoked on
+     *                在其上调用方法的代理实例
      *
      * @param   method the {@code Method} instance corresponding to
      * the interface method invoked on the proxy instance.  The declaring
      * class of the {@code Method} object will be the interface that
      * the method was declared in, which may be a superinterface of the
      * proxy interface that the proxy class inherits the method through.
+     * 对应于在代理实例上调用的接口方法的 Method 实例。
+     * Method 对象的声明类将是在其中声明方法的接口，该接口可以是代理类赖以继承方法的代理接口的超接口。
      *
      * @param   args an array of objects containing the values of the
      * arguments passed in the method invocation on the proxy instance,
@@ -60,6 +72,8 @@ public interface InvocationHandler {
      * Arguments of primitive types are wrapped in instances of the
      * appropriate primitive wrapper class, such as
      * {@code java.lang.Integer} or {@code java.lang.Boolean}.
+     * 包含传入代理实例上方法调用的参数值的对象数组，如果接口方法不使用参数，则为 null。
+     * 基本类型的参数被包装在适当基本包装器类（如 java.lang.Integer 或 java.lang.Boolean）的实例中。
      *
      * @return  the value to return from the method invocation on the
      * proxy instance.  If the declared return type of the interface
@@ -74,6 +88,11 @@ public interface InvocationHandler {
      * the interface method's declared return type as described above,
      * a {@code ClassCastException} will be thrown by the method
      * invocation on the proxy instance.
+     * 从代理实例的方法调用返回的值。
+     * 如果接口方法的声明返回类型是基本类型，则此方法返回的值一定是相应基本包装对象类的实例；
+     * 否则，它一定是可分配到声明返回类型的类型。
+     * 如果此方法返回的值为 null 并且接口方法的返回类型是基本类型，则代理实例上的方法调用将抛出 NullPointerException。
+     * 否则，如果此方法返回的值与上述接口方法的声明返回类型不兼容，则代理实例上的方法调用将抛出 ClassCastException。
      *
      * @throws  Throwable the exception to throw from the method
      * invocation on the proxy instance.  The exception's type must be
@@ -87,6 +106,10 @@ public interface InvocationHandler {
      * {@link UndeclaredThrowableException} containing the
      * exception that was thrown by this method will be thrown by the
      * method invocation on the proxy instance.
+     * 从代理实例上的方法调用抛出的异常。
+     * 该异常的类型必须可以分配到在接口方法的 throws 子句中声明的任一异常类型或未经检查的异常类型 java.lang.RuntimeException 或 java.lang.Error。
+     * 如果此方法抛出经过检查的异常，该异常不可分配到在接口方法的 throws 子句中声明的任一异常类型，
+     * 代理实例的方法调用将抛出包含此方法曾抛出的异常的 UndeclaredThrowableException。
      *
      * @see     UndeclaredThrowableException
      */
